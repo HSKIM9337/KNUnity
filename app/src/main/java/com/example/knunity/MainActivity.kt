1d2d5aa7c3fd96fa65456d833d30345e05389af2
kimhs <hskim9337@naver.com>
2022-10-03 15:22:07 +0900

    amend by HKKIm

--- app/src/main/java/com/example/knunity/MainActivity.kt
+++ app/src/main/java/com/example/knunity/MainActivity.kt
@@ -2,23 +2,10 @@ package com.example.knunity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.knunity.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth : FirebaseAuth
-    private val binding : ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        auth=Firebase.auth
-        binding.logoutBtn.setOnClickListener {
-            auth.signOut()
-            finish()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/knunity/firebaseAuth/JoinActivity.kt
+++ app/src/main/java/com/example/knunity/firebaseAuth/JoinActivity.kt
@@ -1,22 +1,16 @@
 package com.example.knunity.firebaseAuth
 
-import android.annotation.SuppressLint
-import android.content.DialogInterface
 import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
+import android.os.Bundle
+import android.widget.Toast
 import com.example.knunity.MainActivity
+import com.example.knunity.R
+import com.example.knunity.databinding.ActivityIntroBinding
 import com.example.knunity.databinding.ActivityJoinBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-
 
 class JoinActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -33,75 +27,14 @@ class JoinActivity : AppCompatActivity() {
         signup()
 
 
-
-
     }
     //회원가입하기
     //참고 https://firebase.google.com/docs/auth/android/start?hl=ko#kotlin+ktx_1
     // createuserwithemailandpassword 사용하면 됨.
     private fun signup() {
-
-        //최초 linearlayout을 안보이게 만듦으로써, 인증안하면 회원가입 불가능하게 설정
-        binding.checkLinear.visibility=View.INVISIBLE
-        binding.verificationLinear.visibility=View.INVISIBLE
-
-
-        //중복 버튼 눌렀을시
-        binding.checkBtn.setOnClickListener {
-            val email = binding.emailArea.text.toString()
-            if (!email.endsWith("@knu.ac.kr")) {
-                Toast.makeText(this, "경북대학교 웹 메일만 사용 가능합니다", Toast.LENGTH_SHORT).show()
-
-            } else {
-                val builder = AlertDialog.Builder(this)
-                binding.checkBtn.setOnClickListener {
-                    val email = binding.emailArea.text.toString()
-                    auth.fetchSignInMethodsForEmail(email).addOnCompleteListener { task ->
-                        // 새로운 사람인지 확인
-                        val isNewUser = task.result.signInMethods!!.isEmpty()
-                        if (isNewUser) {
-
-                            builder.setMessage(email + "로 인증번호를 보냈습니다")
-                                .setPositiveButton(
-                                    "확인",
-                                    DialogInterface.OnClickListener { dialog, id -> })
-                            builder.create()
-                            builder.show()
-
-                            binding.verificationLinear.visibility = View.VISIBLE
-                            binding.checkLinear.visibility = View.VISIBLE
-                            // 이메일 보내기 추가안함.
-//                        val code = sendEmailVerification()
-//
-//                        if (code.toString() == binding.verificationArea.text.toString()) {
-//
-//                            binding.checkLinear.visibility = View.VISIBLE
-//
-//                        }
-//                        else {
-//                            builder.setMessage("인증번호를 확인해주세요")
-//                                .setPositiveButton("확인",DialogInterface.OnClickListener{dialog,id->})
-//                            builder.create()
-//                            builder.show()
-//                        }
-                        } else {
-                            builder.setMessage("사용 할 수 없는 이메일입니다.")
-                                .setPositiveButton(
-                                    "확인",
-                                    DialogInterface.OnClickListener { dialog, id -> })
-                            builder.create()
-                            builder.show()
-
-                        }
-                    }
-                }
-            }
-        }
         binding.joinBtn.setOnClickListener() {
 
             var isGoToJoin = true
-
-
 
             val email = binding.emailArea.text.toString()
             val password1 = binding.passwordArea1.text.toString()
@@ -125,10 +58,10 @@ class JoinActivity : AppCompatActivity() {
                 isGoToJoin = false
             }
 
-//            if (!email.endsWith("@knu.ac.kr")) {
-//                Toast.makeText(this, "경북대학교 웹 메일만 사용 가능합니다", Toast.LENGTH_SHORT).show()
-//                isGoToJoin = false
-//            }
+            if (!email.endsWith("@knu.ac.kr")) {
+                Toast.makeText(this, "경북대학교 웹 메일만 사용 가능합니다", Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
 
             if (password1.length < 6) {
                 Toast.makeText(this, "비밀번호는 여섯자 이상 입력해주세요", Toast.LENGTH_SHORT).show()
@@ -158,32 +91,4 @@ class JoinActivity : AppCompatActivity() {
 
         }
     }
-    @SuppressLint("QueryPermissionNeeded")
-    private fun sendEmailVerification():Int {
-
-        val range = (0..1000).random()
-        Log.d("range","range")
-
-        val emailAddress = binding.emailArea.text.toString()
-        val title = "KNUnity 인증번호를 알려드립니다"
-
-        Toast.makeText(this,range,Toast.LENGTH_SHORT).show()
-
-        val intent = Intent(Intent.ACTION_SENDTO) // 메일 전송 설정
-            .apply {
-                type = "text/plain" // 데이터 타입 설정
-                data = Uri.parse("mailto:") // 이메일 앱에서만 인텐트 처리되도록 설정
-
-                putExtra(Intent.EXTRA_EMAIL, arrayOf(emailAddress)) // 메일 수신 주소 목록
-                putExtra(Intent.EXTRA_SUBJECT, title) // 메일 제목 설정
-                putExtra(Intent.EXTRA_TEXT, range) // 메일 본문 설정
-            }
-
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(Intent.createChooser(intent, "메일 전송하기"))
-        } else {
-            Toast.makeText(this, "메일을 전송할 수 없습니다", Toast.LENGTH_LONG).show()
-        }
-    return range}
-
 }
