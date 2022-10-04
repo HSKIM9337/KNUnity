5033dc0811de9fd86c0fdce97a1fa4179997fd75
kimhs <hskim9337@naver.com>
2022-10-04 13:50:13 +0900

    Logout fun add - KHS

--- app/src/main/java/com/example/knunity/MainActivity.kt
+++ app/src/main/java/com/example/knunity/MainActivity.kt
@@ -1,28 +1,44 @@
 package com.example.knunity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.knunity.Fragments.*
 import com.example.knunity.databinding.ActivityMainBinding
+import com.example.knunity.firebaseAuth.IntroActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth : FirebaseAuth
-    private val binding : ActivityMainBinding by lazy {
+    private lateinit var auth: FirebaseAuth
+    private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        auth=Firebase.auth
+        auth = Firebase.auth
 //        binding.logoutBtn.setOnClickListener {
 //            auth.signOut()
 //            finish()
 //
 //        }
         setupBottomNavigationView()
+        logout()
+    }
+
+    private fun logout() {
+        binding.settingBtn.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this,"로그아웃하였습니다.",Toast.LENGTH_SHORT).show()
+
+        }
     }
 
     private fun setupBottomNavigationView() {
