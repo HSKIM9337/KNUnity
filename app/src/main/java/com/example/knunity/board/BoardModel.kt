70254f3f2ce844122fcdc63d6c575848a65ac400
jwp10 <mvp_>
2022-11-03 12:43:28 +0900

    Boardlist1st

--- app/src/main/java/com/example/knunity/board/BoardModel.kt
+++ app/src/main/java/com/example/knunity/board/BoardModel.kt
@@ -1,8 +1,11 @@
 package com.example.knunity.board
+
+import java.io.Serializable
+
 //
 data class BoardModel (
     val uid: String="",
     val title: String="",
     val contents: String="",
     val time : String=""
-)
+): Serializable
--- app/src/main/java/com/example/knunity/board/MyViewHolder.kt
+++ app/src/main/java/com/example/knunity/board/MyViewHolder.kt
@@ -1,18 +1,25 @@
 package com.example.knunity.board
 
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import com.example.knunity.databinding.BoardListItemViewBinding
 
 class MyViewHolder(private val binding: BoardListItemViewBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
+    private val context = binding.root.context
         fun bind(data:BoardModel) {
             with(binding) {
                 titleTv.text = data.title
                 contentsTv.text = data.contents
                 timeTv.text = data.time
                 uidTv.text =data.uid
-
+                itemView.setOnClickListener{
+                    val intent = Intent(context, BoardInsideActivity::class.java).apply {
+                        putExtra("data",data)
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    }.run { context.startActivity(this) }
+                }
             }
         }
 }
