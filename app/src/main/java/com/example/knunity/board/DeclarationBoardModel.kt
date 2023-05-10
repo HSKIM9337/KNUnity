bb77bd38727801960c8cc2ba002ccd302f4e7bd5
kimhs <hskim9337@naver.com>
2023-05-10 18:36:19 +0900

    1. 이상한커밋변경

--- app/src/main/java/com/example/knunity/board/DeclarationBoardModel.kt
+++ app/src/main/java/com/example/knunity/board/DeclarationBoardModel.kt
@@ -1,16 +1,8 @@
-<<<<<<<< HEAD:app/src/main/java/com/example/knunity/comment/CommentBoardModel.kt
-package com.example.knunity.comment
-
-import java.io.Serializable
-
-data class CommentBoardModel(
-========
 package com.example.knunity.board
 
 import java.io.Serializable
 
 data class DeclarationBoardModel(
->>>>>>>> origin/jwptest:app/src/main/java/com/example/knunity/board/DeclarationBoardModel.kt
     val key: String="",
     val userUid: String="",
     val title: String="",
